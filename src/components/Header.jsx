ce682bc5c385b8d42ba01adf8f082cb00508a149
jaeyeong815 <jae0ee@naver.com>
2022-10-30 02:22:03 +0900

    style(header): navigate 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Header = () => {
+  const navigate = useNavigate();
   const OWNER = process.env.REACT_APP_OWNER;
   const REPO = process.env.REACT_APP_REPO;
 
   return (
-    <Title>
+    <Title onClick={() => navigate('/', { replace: true })}>
       <h3>
         {OWNER} / {REPO}
       </h3>
@@ -18,8 +20,11 @@ export default Header;
 
 const Title = styled.header`
   text-align: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  cursor: pointer;
+
   h3 {
     font-size: 25px;
   }
-  margin-bottom: 40px;
 `;
