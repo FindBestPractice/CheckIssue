bd642c9bda0b9d613fd942af711a2bae38f4ce07
jaeyeong815 <jae0ee@naver.com>
2022-10-29 23:55:37 +0900

    feat(header): 공통 헤더 필수 표시항목 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Header = () => {
+  const OWNER = process.env.REACT_APP_OWNER;
+  const REPO = process.env.REACT_APP_REPO;
+
   return (
     <Title>
-      <h3>Organization Name / Repository Name</h3>
+      <h3>
+        {OWNER} / {REPO}
+      </h3>
     </Title>
   );
 };
