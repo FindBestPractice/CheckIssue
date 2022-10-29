cbfa2447b0206ca7efcc1eafa9a26bcc0c9f7373
jaeyeong815 <jae0ee@naver.com>
2022-10-29 23:56:59 +0900

    feat(List): context api 사용하여 데이터 받기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import GlobalStyle from './shared/GlobalStyle';
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import Routers from './Router';
+import axios from 'axios';
 
 export const IssueContext = createContext('defaultValue');
 
 const App = () => {
+  const TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
+  const OWNER = process.env.REACT_APP_OWNER;
+  const REPO = process.env.REACT_APP_REPO;
+
+  const [issueData, setIssueData] = useState([]);
+
+  const getIssueList = async () => {
+    await axios
+      .get(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
+        headers: {
+          Authorization: `Basic ${TOKEN}`,
+        },
+      })
+      .then((res) => setIssueData(res.data));
+  };
+  useEffect(() => {
+    getIssueList();
+  }, []);
+
   return (
-    <IssueContext.Provider value="test">
+    <IssueContext.Provider value={issueData}>
       <GlobalStyle />
       <Routers />
     </IssueContext.Provider>
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,15 +1,46 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import Header from '../components/Header';
 import { IssueContext } from '../App';
 
 const List = () => {
-  const value = useContext(IssueContext);
+  const issueList = useContext(IssueContext);
+
   return (
     <div className="List">
       <Header />
-      {value}
+      {issueList &&
+        issueList.map((issue) => {
+          return (
+            <IssueWrapper key={issue.id}>
+              <IssueInfo>
+                <div className="issueTitle">
+                  <p className="issueNumber">#{issue.number}</p>
+                  <span className="title">{issue.title}</span>
+                </div>
+                <div className="issueInfo">
+                  <span className="writer">작성자: {issue.user.login},</span>
+                  <span className="date">작성일: {issue.created_at},</span>
+                </div>
+              </IssueInfo>
+              <div className="comment">코멘트: {issue.comments}</div>
+            </IssueWrapper>
+          );
+        })}
     </div>
   );
 };
 
 export default List;
+
+const IssueWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const IssueInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
