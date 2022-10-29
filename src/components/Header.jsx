0e174c26e247a5c1bee9aa2715f5981269d660e9
jaeyeong815 <jae0ee@naver.com>
2022-10-29 22:15:40 +0900

    feat(header): Header 컴포넌트 문구 임시 작성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
-  return <div>헤더</div>;
+  return (
+    <Title>
+      <h3>Organization Name / Repository Name</h3>
+    </Title>
+  );
 };
 
 export default Header;
+
+const Title = styled.header`
+  h3 {
+    font-size: 25px;
+  }
+  margin-bottom: 40px;
+`;
