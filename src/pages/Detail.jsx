c597af67a13be8d907134bb9984ea4e41694cf4b
jaeyeong815 <jae0ee@naver.com>
2022-10-30 12:13:08 +0900

    feat(Detail): Detail 컴포넌트 로딩중 상태 표시

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import axios from 'axios';
-import styled from 'styled-components';
-import Header from '../components/Header';
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import styled from 'styled-components';
+import axios from 'axios';
+import Header from '../components/Header';
+import Loding from '../components/Loding';
 
 const Detail = () => {
   const { issue } = useParams();
   const [issueInfo, setIssueInfo] = useState({});
+  const [isLoding, setIsLoding] = useState(true);
   const TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
   const OWNER = process.env.REACT_APP_OWNER;
   const REPO = process.env.REACT_APP_REPO;
 
+  // FIXME 마크다운 화면 수정필요
   const getIssueDetail = async (issueNumber) => {
     await axios
       .get(`https://api.github.com/repos/${OWNER}/${REPO}/issues/${issueNumber}`, {
@@ -20,7 +23,10 @@ const Detail = () => {
           Authorization: `Basic ${TOKEN}`,
         },
       })
-      .then((res) => setIssueInfo(res.data));
+      .then((res) => {
+        setIssueInfo(res.data);
+        setIsLoding(false);
+      });
   };
 
   useEffect(() => {
@@ -30,24 +36,28 @@ const Detail = () => {
   return (
     <div className="Detail">
       <Header />
-      {issueInfo && (
-        <>
-          <InfoHeader>
-            <img src={issueInfo.user?.avatar_url} alt="profileImg" />
-            <TitleWrapper>
-              <div className="title">
-                <p className="number">#{issueInfo.number}</p>
-                <p className="title">{issueInfo.title}</p>
-              </div>
-              <div className="info">
-                <span className="writer"> 작성자 {issueInfo.user?.login}</span>
-                <span className="date"> 작성일 {issueInfo.created_at}</span>
-              </div>
-            </TitleWrapper>
-            <div>코멘트: {issueInfo.comments}</div>
-          </InfoHeader>
-          <InfoBody>본문 {issueInfo.body}</InfoBody>
-        </>
+      {isLoding ? (
+        <Loding />
+      ) : (
+        issueInfo && (
+          <>
+            <InfoHeader>
+              <img src={issueInfo.user?.avatar_url} alt="profileImg" />
+              <TitleWrapper>
+                <div className="title">
+                  <p className="number">#{issueInfo.number}</p>
+                  <p className="title">{issueInfo.title}</p>
+                </div>
+                <div className="info">
+                  <span className="writer"> 작성자 {issueInfo.user?.login}</span>
+                  <span className="date"> 작성일 {issueInfo.created_at}</span>
+                </div>
+              </TitleWrapper>
+              <div>코멘트: {issueInfo.comments}</div>
+            </InfoHeader>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{issueInfo.body}</ReactMarkdown>
+          </>
+        )
       )}
     </div>
   );
@@ -62,12 +72,15 @@ const InfoHeader = styled.div`
 
   img {
     width: 50px;
+    border-radius: 50%;
+    margin-right: 20px;
   }
 `;
 
 const TitleWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  flex-grow: 2;
 
   margin-right: 20px;
 
