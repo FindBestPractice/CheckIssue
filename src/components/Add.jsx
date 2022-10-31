3cdd837478795a4513f6497fc977c83d25206de1
jaeyeong815 <jae0ee@naver.com>
2022-10-31 14:35:55 +0900

    feat(add): 광고 이미지 컴포넌트 추가

---
+++ src/components/Add.jsx
@@ -0,0 +1,10 @@
+const Add = () => {
+  return (
+    <img
+      src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100"
+      alt="addImg"
+    />
+  );
+};
+
+export default Add;
